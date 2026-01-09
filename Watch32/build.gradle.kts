b6abf7325c9c2c34975320d832f2ee967cbe1301
kerimmkirac <kerimmkirac@gmail.com>
2026-01-09 16:37:44 +0300

    restart

---
+++ Watch32/build.gradle.kts
@@ -0,0 +1,31 @@
+version = 6
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+
+cloudstream {
+    
+
+    description = "Watch Your Favorite Movies TV Shows Online - Streaming For Free. With Movies TV Shows Full HD. Find Your Movies Watch NOW!"
+    language = "en"
+    authors = listOf("kerimmkirac")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1
+
+    tvTypes = listOf("Movie","TvSeries")
+
+
+    
+    iconUrl = "https://img.watch32.sx/xxrz/400x400/100/a9/5e/a95e15a880a9df3c045f6a5224daf576/a95e15a880a9df3c045f6a5224daf576.png"
+}
